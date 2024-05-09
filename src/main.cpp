fdedffc99c6d2894191983005cf88107aea5bd6a
kkj-100-010-110 <gjk.100.010.110@gmail.com>
2024-05-10 05:27:27 +0900

    Camera
    - Camera done

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,25 @@ void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
 // window의 프레임 버퍼 크기가 변경되었을 때 호출하기 위한 콜백 정의
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height)
 {
+    // SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    // glViewport(0, 0, width, height);
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    glViewport(0, 0, width, height);
+    auto context = reinterpret_cast<Context*>(glfwGetWindowUserPointer(window));
+    context->Reshape(width, height);
+}
+
+void OnCursorPos(GLFWwindow* window, double x, double y)
+{
+    auto context = (Context*)glfwGetWindowUserPointer(window);
+    context->MouseMove(x, y);
+}
+
+void OnMouseButton(GLFWwindow* window, int button, int action, int modifier)
+{
+    auto context = (Context*)glfwGetWindowUserPointer(window);
+    double x, y;
+    glfwGetCursorPos(window, &x, &y);
+    context->MouseButton(button, action, x, y);
 }
 
 int main(int argc, char* argv[])
@@ -70,18 +87,24 @@ int main(int argc, char* argv[])
         glfwTerminate();
         return -1;
     }
+    glfwSetWindowUserPointer(window, context.get());
 
     OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
     // glfw에게 프레임 버퍼 사이즈 변경 시 현재 컨텍스트에서 'OnFramebufferSizeChange' 콜백 요청
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
     // glfw에게 키 이벤트 발생 시 현재 컨텍스트에서 'OnKeyEvent' 콜백 요청
     glfwSetKeyCallback(window, OnKeyEvent);
+    // glfw에게 마우스 커서의 움직임에 따른 'OnCursorPos' 콜백 요청
+    glfwSetCursorPosCallback(window, OnCursorPos);
+    // glfw에게 마우스 버튼이 클릭이 되어있을 때 'OnMouseButton' 콜백 요청
+    glfwSetMouseButtonCallback(window, OnMouseButton);
 
     SPDLOG_INFO("OpenGL context version: {}", glVersion);
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        context->ProcessInput(window);
         context->Render();
         glfwSwapBuffers(window);
     }
