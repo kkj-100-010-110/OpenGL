9249857f58128e73831ddf7c0655aa8f47a2d9fd
kkj-100-010-110 <gjk.100.010.110@gmail.com>
2024-05-10 09:09:55 +0900

    IMGUI
    - add imgui

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,23 @@
 #include <spdlog/spdlog.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+void OnCharEvent(GLFWwindow* window, unsigned int ch)
+{
+    ImGui_ImplGlfw_CharCallback(window, ch);
+}
+
+void OnScroll(GLFWwindow* window, double xoffset, double yoffset)
+{
+    ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
+}
 
 // window에서 키 이벤트 발생 시 호출하기 위한 콜백 정의
 void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
     SPDLOG_INFO(
         "key: {}, scancode: {}, action: {}, mods: {}{}{}", key, scancode,
         action == GLFW_PRESS ? "Pressed"
@@ -38,6 +51,7 @@ void OnCursorPos(GLFWwindow* window, double x, double y)
 
 void OnMouseButton(GLFWwindow* window, int button, int action, int modifier)
 {
+    ImGui_ImplGlfw_MouseButtonCallback(window, button, action, modifier);
     auto context = (Context*)glfwGetWindowUserPointer(window);
     double x, y;
     glfwGetCursorPos(window, &x, &y);
@@ -81,12 +95,21 @@ int main(int argc, char* argv[])
     }
     auto glVersion = (char*)glGetString(GL_VERSION);
 
+    // create imgui context
+    auto imguiContext = ImGui::CreateContext();
+    ImGui::SetCurrentContext(imguiContext);
+    ImGui_ImplGlfw_InitForOpenGL(window, false);
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplOpenGL3_CreateFontsTexture();
+    ImGui_ImplOpenGL3_CreateDeviceObjects();
+
     auto context = Context::Create();
     if (!context) {
         SPDLOG_ERROR("error: failed to create context");
         glfwTerminate();
         return -1;
     }
+    // 전역 변수 사용 대신 glfw에 context 포인터를 설정하여 사용하기 위한 함수
     glfwSetWindowUserPointer(window, context.get());
 
     OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -94,21 +117,39 @@ int main(int argc, char* argv[])
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
     // glfw에게 키 이벤트 발생 시 현재 컨텍스트에서 'OnKeyEvent' 콜백 요청
     glfwSetKeyCallback(window, OnKeyEvent);
+
+    glfwSetCharCallback(window, OnCharEvent);
+
     // glfw에게 마우스 커서의 움직임에 따른 'OnCursorPos' 콜백 요청
     glfwSetCursorPosCallback(window, OnCursorPos);
     // glfw에게 마우스 버튼이 클릭이 되어있을 때 'OnMouseButton' 콜백 요청
     glfwSetMouseButtonCallback(window, OnMouseButton);
+
+    glfwSetScrollCallback(window, OnScroll);
 
     SPDLOG_INFO("OpenGL context version: {}", glVersion);
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
         context->ProcessInput(window);
         context->Render();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
     context.reset(); // or context = nullptr;
+
+    ImGui_ImplOpenGL3_DestroyFontsTexture();
+    ImGui_ImplOpenGL3_DestroyDeviceObjects();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext(imguiContext);
 
     glfwTerminate();
     return 0;
